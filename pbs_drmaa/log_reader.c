77e4bc643b9ca4532d751161e36e56fe9c610b87
mmamonski <mmamonski@8090e593-4c54-45a0-9512-3a60eea62f94>
2011-10-26 15:32:11 +0000

    pbs pro parsing vnodes

--- pbs_drmaa/log_reader.c
+++ pbs_drmaa/log_reader.c
@@ -312,13 +312,9 @@ pbsdrmaa_read_log( pbsdrmaa_log_reader_t * self )
 #ifdef PBS_PROFESSIONAL
 									{
 										char *p_vnode = NULL;
-										if ((p_vnode = strstr(field, "exec_vnode")))
+										if ((p_vnode = strstr(msg, "exec_vnode")))
 										 {
-											last_attr->next = &struct_exec_vnode;
-											last_attr =  &struct_exec_vnode;
-											struct_exec_vnode.name = "exec_vnode";
-											struct_exec_vnode.next = NULL;
-											struct_exec_vnode.value = fsd_strdup(p_vnode + 11);
+											attribs = pbsdrmaa_add_attr(attribs, PBSDRMAA_EXECUTION_VNODE, p_vnode + 11);
 										 }
 									}
 #endif
